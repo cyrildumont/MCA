578d514e24b621c7edab797ebd696e531155fdaf
dumont.cyril@gmail.com <dumont.cyril@gmail.com@4d451efd-e0e9-feca-6a6e-7f20c84de37d>
2011-05-20 09:17:47 +0000

    git-svn-id: http://mca-project.googlecode.com/svn/trunk@184 4d451efd-e0e9-feca-6a6e-7f20c84de37d

--- org.mca.mca-parent/mca-core/src/main/java/org/mca/util/MCAUtils.java
+++ org.mca.mca-parent/mca-core/src/main/java/org/mca/util/MCAUtils.java
@@ -2,36 +2,62 @@ package org.mca.util;
 
 import java.net.InetAddress;
 import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Enumeration;
+import java.util.List;
 
 public abstract class MCAUtils {
+
+
+	public static NetworkInterface[] getNetworkInterfaces(){
+		List<NetworkInterface> interfaces = new ArrayList<NetworkInterface>();
+		try {
+			Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces();
+			while (en.hasMoreElements()) {
+				NetworkInterface interfaceN = (NetworkInterface)en.nextElement(); 
+				Enumeration<InetAddress> ienum = interfaceN.getInetAddresses();
+				while (ienum.hasMoreElements()) {
+					InetAddress ia = ienum.nextElement();
+					String adress = ia.getHostAddress().toString();     
+					System.out.println(adress);
+				}
+				System.out.println("-------");
+			}
+		} catch (SocketException e) {
+			e.printStackTrace();
+		}
+		return (NetworkInterface[])interfaces.toArray(new NetworkInterface[interfaces.size()]);
+	}
+
 
 	/**
 	 * 
 	 * @return
 	 */
-	
+
 	public static String getIP(){
 		try{
 			Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
-		                
-	         while (interfaces.hasMoreElements()) {
-	            NetworkInterface interfaceN = (NetworkInterface)interfaces.nextElement(); 
-	            Enumeration<InetAddress> ienum = interfaceN.getInetAddresses();
-	            while (ienum.hasMoreElements()) {
-	                InetAddress ia = ienum.nextElement();
-	                String adress = ia.getHostAddress().toString();     
-	                if(adress.length() < 16 && !adress.startsWith("127") && !(adress.indexOf(":") > 0)){
-	                    	  return adress;        
-	                    }
-	            }
-	        }
-	 		return "127.0.0.1";
+
+			while (interfaces.hasMoreElements()) {
+				NetworkInterface interfaceN = (NetworkInterface)interfaces.nextElement(); 
+				Enumeration<InetAddress> ienum = interfaceN.getInetAddresses();
+				while (ienum.hasMoreElements()) {
+					InetAddress ia = ienum.nextElement();
+					String adress = ia.getHostAddress().toString();     
+					if(adress.length() < 16 && !adress.startsWith("127") && !(adress.indexOf(":") > 0)){
+						return adress;        
+					}
+				}
+			}
+			return "127.0.0.1";
 		}catch(Exception e){
-	        System.out.println("pas de carte reseau");
-	        e.printStackTrace();
-	        return null;
-	    }
-	    
+			System.out.println("pas de carte reseau");
+			e.printStackTrace();
+			return null;
+		}
+
 	}
 }
