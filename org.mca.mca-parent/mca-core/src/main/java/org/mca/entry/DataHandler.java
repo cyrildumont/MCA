aac82f725b385e5904d4c7d15716ed1233fbd182
dumont.cyril@gmail.com <dumont.cyril@gmail.com@4d451efd-e0e9-feca-6a6e-7f20c84de37d>
2011-09-02 10:17:34 +0000

    git-svn-id: http://mca-project.googlecode.com/svn/trunk@229 4d451efd-e0e9-feca-6a6e-7f20c84de37d

--- org.mca.mca-parent/mca-core/src/main/java/org/mca/entry/DataHandler.java
+++ org.mca.mca-parent/mca-core/src/main/java/org/mca/entry/DataHandler.java
@@ -9,13 +9,13 @@ import org.apache.commons.net.io.Util;
 import org.w3c.dom.Node;
 
 public class DataHandler extends Storable{
-	 
+
 	private static final long serialVersionUID = -2138412094431779435L;
-	
+
 	public String name;
-	
+
 	public String worker;
-	
+
 	public DataHandler() {}
 
 	/**
@@ -25,30 +25,48 @@ public class DataHandler extends Storable{
 	 */
 	public File download(String dir) throws IOException{
 		String dest = dir + "/" + name +".dat";
-		File file = new File(dest);
-		FileOutputStream out = new FileOutputStream(file);
-		InputStream stream = getInputStream();
-		Util.copyStream(stream, out, 1024);
-		stream.close();
-		close();
+		final File file = new File(dest);
+		new Thread(new Runnable() {	
+			@Override
+			public void run(){
+				try {
+					FileOutputStream out = new FileOutputStream(file);
+					InputStream stream = getInputStream();
+					Util.copyStream(stream, out, 1024);
+					stream.close();
+					close();
+				}catch (IOException e) {
+					e.printStackTrace();
+				}	
+			}
+		}).start();
 		return file;
 	}
-	
+
 	/**
 	 * 
 	 * @param input
 	 */
-	public void upload(InputStream input) throws IOException{
-		OutputStream stream = getOutputStream();
-		Util.copyStream(input, stream);
-		stream.close();
-		close();
+	public void upload(final InputStream input) throws IOException{
+		new Thread(new Runnable() {	
+			@Override
+			public void run(){
+				try {
+					OutputStream stream = getOutputStream();
+					Util.copyStream(input, stream);
+					stream.close();
+					close();
+				}catch (IOException e) {
+					e.printStackTrace();
+				}	
+			}
+		}).start();
 	}
-	
+
 	protected InputStream getInputStream(){return null;}
-	
+
 	protected OutputStream getOutputStream(){return null;}
-	
+
 	protected void close(){}
 
 	@Override
