00e4cff0cc65909f48e8058e58f69d3b9b19bc95
dumont.cyril@gmail.com <dumont.cyril@gmail.com@4d451efd-e0e9-feca-6a6e-7f20c84de37d>
2011-10-18 15:21:45 +0000

    git-svn-id: http://mca-project.googlecode.com/svn/trunk@291 4d451efd-e0e9-feca-6a6e-7f20c84de37d

--- org.mca.mca-parent/mca-core/src/main/java/org/mca/masterworker/MWConstants.java
+++
@@ -1,9 +0,0 @@
-package org.mca.masterworker;
-
-public interface MWConstants {
-
-	public static final String MASTER_TASK_NAME = "master";
-	
-	public static final String WORKER_TASK_NAME = "worker";
-	
-}
