a38507fff3097d094c028b66f259472cca8dce01
dumont.cyril@gmail.com <dumont.cyril@gmail.com@4d451efd-e0e9-feca-6a6e-7f20c84de37d>
2011-11-05 14:06:21 +0000

    git-svn-id: http://mca-project.googlecode.com/svn/trunk@302 4d451efd-e0e9-feca-6a6e-7f20c84de37d

--- org.mca.mca-parent/mca-server/src/main/bin/mcaserver.sh
+++ org.mca.mca-parent/mca-server/src/main/bin/mcaserver.sh
@@ -27,7 +27,7 @@ MCA_MAIN="org.mca.startup.Bootstrap"
 OPTIONS=" -wait 10 -user $MCA_USER -Dmca.home=$MCA_HOME -Djava.security.policy=$MCA_HOME/conf/security/server.policy -Dcom.sun.management.jmxremote.port=9099 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Djava.util.logging.config.file=$MCA_HOME/conf/logging.properties -Djavax.net.ssl.trustStore=$MCA_HOME/conf/security/keystore.server -Djava.security.auth.login.config=$MCA_HOME/conf/security/server.login -cp ./commons-daemon.jar:./bootstrap.jar:./commons-logging-1.0.4.jar:./log4j-1.2.14.jar:../conf/ -outfile $MCA_HOME/logs/mcaserver.out -errfile $MCA_HOME/logs/mcaserver.err -pidfile $PIDFILE"
   	
 start(){ 
-	echo -n "Starting MCASercer daemon: " 
+	echo -n "Starting MCAServer daemon: " 
 	#modprobe capability 
 	cd $MCA_HOME/bin
 	./jsvc $OPTIONS $MCA_MAIN $MCA_HOME/conf/mca-server.xml
